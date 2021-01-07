1c6f92214233d9602ea8334e375eecd918ba89ea
J. Sterling Bishop <jsterlingbishop@gmail.com>
2021-01-07 12:08:26 -0800

    companies rotues

--- routes/companies.js
+++ routes/companies.js
@@ -3,7 +3,7 @@ const router = new express.Router();
 const db = require("../db");
 const { NotFoundError } = require("../expressError");
 
-/** Returns a list of all the companies. */
+/** GET /companies Returns a list of all the companies. */
 router.get("/",
   async function (req, res, next) {
     const results = await db.query(
@@ -14,7 +14,7 @@ router.get("/",
     return res.json({ companies });
   });
 
-/** Returns a company based on the URL parameter. */
+/** GET /companies/[code] Returns a company based on the URL parameter. */
 router.get("/:code",
   async function (req, res, next) {
     let code = req.params.code;
@@ -31,49 +31,50 @@ router.get("/:code",
     return res.json({ company });
   });
 
+/* POST /companies Returns a newly created company*/
+router.post("/",
+  async function (req, res, next) {
+    let { code, name, description } = req.body;
 
+    const result = await db.query(
+      `INSERT INTO companies (code, name, description)
+              VALUES ($1, $2, $3)
+              RETURNING code, name, description`,
+      [code, name, description]);
+    const company = result.rows[0];
+    return res.status(201).json({ company });
+  });
 
-// /** TEMPLATE EXAMPLE POSTGRES ROUTE*/
-// /** Create new user, return user */
-// router.post("/", async function (req, res, next) {
-//   const { name, type } = req.body;
+/*  PUT /companies/[code] Returns an updated company */
+router.put("/:code",
+  async function (req, res, next) {
+    let code = req.params.code;
+    let { name, description } = req.body;
 
-//   const result = await db.query(
-//     `INSERT INTO users (name, type)
-//            VALUES ($1, $2)
-//            RETURNING id, name, type`,
-//     [name, type],
-//   );
-//   const user = result.rows[0];
-//   return res.status(201).json({ user });
-// });
+    const result = await db.query(
+      `UPDATE companies
+              SET name=$2,
+                  description=$3
+              WHERE code=$1
+              RETURNING code, name, description`,
+      [code, name, description]);
+    const company = result.rows[0];
+    if (!company) throw new NotFoundError();
 
-// /** TEMPLATE EXAMPLE POSTGRES ROUTE*/
-// /** Update user, returning user */
-// router.patch("/:id", async function (req, res, next) {
-//   const { name, type } = req.body;
+    return res.json({ company });
+  });
 
-//   const result = await db.query(
-//     `UPDATE users
-//            SET name=$1,
-//                type=$2
-//            WHERE id = $3
-//            RETURNING id, name, type`,
-//     [name, type, req.params.id],
-//   );
-//   const user = result.rows[0];
-//   return res.json({ user });
-// });
-
-
-// /** TEMPLATE EXAMPLE POSTGRES ROUTE*/
-// /** Delete user, returning {message: "Deleted"} */
-// router.delete("/:id", async function (req, res, next) {
-//   await db.query(
-//     "DELETE FROM users WHERE id = $1",
-//     [req.params.id],
-//   );
-//   return res.json({ message: "Deleted" });
-// });
+/* DELETE /companies/[code] Returns {status: deleted} or 404 */
+router.delete("/:code",
+  async function (req, res, next) {
+    let code = req.params.code;
+    const result = await db.query(
+      `DELETE FROM companies
+       WHERE code=$1
+       RETURNING code`, [code]);
+    const company = result.rows[0];
+    if (!company) throw new NotFoundError();
+    return res.json({ "status": "deleted" });
+  });
 
 module.exports = router;
